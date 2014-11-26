abee8579c7476958073c76d4fa079a8d3873bf7c
Chaosvex <ch@osvex.com>
2014-11-26 17:02:35 +0000

    Arty-farty banner, now with colours

--- src/shared/Banner.cpp
+++ src/shared/Banner.cpp
@@ -2,20 +2,36 @@
 
 #include "Banner.h"
 #include "Version.h"
+#include "rlutil.h"
 #include <iostream>
 
 namespace ember {
 
 void print_banner(const std::string& display_name) {
+	rlutil::saveColor();
+
+	rlutil::setColor(rlutil::DARKGREY);
 	std::cout << "\n"
-		R"(                                      d8b                       )" << "\n"
-		R"(                                      ?88                       )" << "\n"
-		R"(                                       88b                      )" << "\n"
-		R"(                 d8888b  88bd8b,d88b   888888b  d8888b  88bd88b )" << "\n"
-		R"(                d8b_,dP  88P'`?8P'?8b  88P `?8bd8b_,dP  88P'  ` )" << "\n"
-		R"(                88b     d88  d88  88P d88,  d8888b     d88      )" << "\n"
+		R"(                                      d8b                       )" << "\n";
+	rlutil::setColor(rlutil::GREY);
+	std::cout <<
+		R"(                                      ?88                       )" << "\n";
+	rlutil::setColor(rlutil::YELLOW);
+
+	std::cout <<
+		R"(                                       88b                      )" << "\n";
+	std::cout <<
+		R"(                 d8888b  88bd8b,d88b   888888b  d8888b  88bd88b )" << "\n";
+	rlutil::setColor(rlutil::LIGHTRED);
+	std::cout <<
+		R"(                d8b_,dP  88P'`?8P'?8b  88P `?8bd8b_,dP  88P'  ` )" << "\n";
+	rlutil::setColor(rlutil::RED);
+	std::cout <<
+		R"(                88b     d88  d88  88P d88,  d8888b     d88      )" << "\n";
+	std::cout <<
 		R"(                `?888P'd88' d88'  88bd88'`?88P'`?888P'd88'      )" << "\n\n";
-		
+
+	rlutil::resetColor();
 	std::cout << display_name << ", v" << version::VERSION << " (" << version::GIT_HASH << ")\n\n";
 }
 
