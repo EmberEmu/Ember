6943cdbce4ea6086af1e1a46aad781af0df295a3
Chaosvex <ch@osvex.com>
2015-01-24 23:26:05 +0000

    Workaround for VS2013 bug Fixed deadlock in wait() Added maximum value option

--- src/shared/threading/Semaphore.h
+++ src/shared/threading/Semaphore.h
@@ -18,11 +18,28 @@ template<typename Lock>
 class Semaphore {
 	std::atomic<unsigned int> count_;
 	std::condition_variable condition_;
+	unsigned int max_;
 	Lock lock_;
 
+	bool increment_max_check(unsigned int increment) {
+		unsigned int curr = count_;
+		unsigned int new_count;
+
+		do {
+			new_count = curr + increment;
+
+			if(new_count < curr || new_count > max_) { //check for overflow
+				return false;
+			}
+		} while(!count_.compare_exchange_weak(curr, new_count));
+		return true;
+	}
+
 public:
-	Semaphore(unsigned int initial_count = 0) {
-		count_ = initial_count;
+	Semaphore(unsigned int initial_count = 0,
+	          unsigned int max_count = std::numeric_limits<unsigned int>::max())
+	          : count_(initial_count) {
+		max_ = max_count;
 	}
 
 	void wait() {
@@ -38,24 +55,34 @@ public:
 	bool wait_for(std::chrono::milliseconds duration) {
 		std::unique_lock<Lock> guard(lock_);
 
-		return !condition_.wait_for(guard, std::chrono::milliseconds(duration),
-			[this]() {
+		//wait_for with a predicate is broken in VS2013 & VS2015 Preview - workaround! todo
+		bool hack = false;
+
+		condition_.wait_for(guard, std::chrono::milliseconds(duration),
+			[this, &hack]() {
 				if(count_ != 0) {
 					--count_;
+					hack = true;
 					return true;
 				}
 				return false;
 			});
+
+		return hack;
 	}
 
-	void signal() {
-		++count_;
+	bool signal(unsigned int increment = 1) {
+		bool ret = increment_max_check(increment); 
+		std::lock_guard<Lock> guard(lock_);
 		condition_.notify_one();
+		return ret;
 	}
 
-	void signal_all() {
-		++count_;
+	bool signal_all(unsigned int increment = 1) {
+		bool ret = increment_max_check(increment);
+		std::lock_guard<Lock> guard(lock_);
 		condition_.notify_all();
+		return ret;
 	}
 };
 
