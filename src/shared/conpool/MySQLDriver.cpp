9c9836f8ea996533c3fb370f4f3261bc9ce8998b
Chaosvex <ch@osvex.com>
2015-02-01 00:49:32 +0000

    Add a basic prepared statement cache to the MySQL driver

--- src/shared/conpool/MySQLDriver.cpp
+++ src/shared/conpool/MySQLDriver.cpp
@@ -15,6 +15,7 @@
 #include <cppconn/prepared_statement.h>
 #include <memory>
 #include <sstream>
+#include <iostream>
 
 namespace ember { namespace drivers {
 
@@ -36,11 +37,20 @@ sql::Connection* MySQL::open() const {
 void MySQL::close(sql::Connection* conn) const {
 	conn->close();
 	delete conn;
+
+	auto conn_cache = locate_cache(conn);
+
+	if(conn_cache) {
+		for(auto stmt : *conn_cache) {
+			stmt.second->close();
+			delete stmt.second;
+		}
+	}
 }
 
 sql::Connection* MySQL::keep_alive(sql::Connection* conn) const try {
-	std::unique_ptr<sql::Statement> stmt(conn->createStatement());
 	conn->setAutoCommit(true);
+	std::unique_ptr<sql::Statement> stmt(conn->createStatement());
 	stmt->execute("/* ping */");
 	return conn;
 } catch(sql::SQLException&) {
@@ -68,6 +78,39 @@ std::string MySQL::version() const {
 	ver << driver->getMajorVersion() << "." << driver->getMinorVersion() << "."
 	    << driver->getPatchVersion();
 	return ver.str();
+}
+
+sql::PreparedStatement* MySQL::lookup_statement(const sql::Connection* conn, const std::string& key) const {
+	auto conn_cache = locate_cache(conn);
+
+	if(!conn_cache) {
+		return nullptr;
+	}
+
+	auto conn_cache_it = conn_cache->find(key);
+	
+	if(conn_cache_it == conn_cache->end()) {
+		return nullptr;
+	}
+
+	return conn_cache_it->second;
+}
+
+void MySQL::cache_statement(const sql::Connection* conn, const std::string& key,
+                            sql::PreparedStatement* value) {
+	std::lock_guard<std::mutex> lock(cache_lock_);
+	cache_[conn][key] = value;
+}
+
+QueryCache* MySQL::locate_cache(const sql::Connection* conn) const {
+	std::lock_guard<std::mutex> lock(cache_lock_);
+	auto cache_it = cache_.find(conn);
+
+	if(cache_it == cache_.end()) {
+		return nullptr;
+	}
+
+	return &cache_it->second;
 }
 
 }} // drivers, ember
--- src/shared/conpool/include/conpool/drivers/MySQLDriver.h
+++ src/shared/conpool/include/conpool/drivers/MySQLDriver.h
@@ -9,24 +9,33 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+#include <mutex>
 
 namespace sql {
 
 class Driver;
 class Connection;
+class PreparedStatement;
 
 }
 
 namespace ember { namespace drivers {
 
+typedef std::unordered_map<std::string, sql::PreparedStatement*> QueryCache;
+
 class MySQL {
+	
+
 	const std::string dsn, username, password, database;
 	sql::Driver* driver;
+	mutable std::unordered_map<const sql::Connection*, QueryCache> cache_;
+	mutable std::mutex cache_lock_;
 
 public:
 	MySQL(std::string user, std::string password, const std::string& host,
 	      unsigned short port, std::string db);
-
+	
 	sql::Connection* open() const;
 	sql::Connection* clean(sql::Connection* conn) const;
 	void close(sql::Connection* conn) const;
@@ -36,6 +45,10 @@ public:
 	void thread_exit() const;
 	std::string name() const;
 	std::string version() const;
+	QueryCache* locate_cache(const sql::Connection* conn) const;
+	sql::PreparedStatement* lookup_statement(const sql::Connection* conn, const std::string& key) const;
+	void cache_statement(const sql::Connection* conn, const std::string& key,
+	                     sql::PreparedStatement* value);
 };
 
 }} //drivers, ember
