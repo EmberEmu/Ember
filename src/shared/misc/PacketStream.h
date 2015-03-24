2360453dab974a090e3b834fac7a82f79dfb9a12
Chaosvex <ch@osvex.com>
2015-03-24 05:59:54 +0000

    Allow for buffer to be swapped

--- src/shared/misc/PacketStream.h
+++ src/shared/misc/PacketStream.h
@@ -12,10 +12,10 @@ typedef std::vector<char> Packet;
 
 template<typename T>
 class PacketStream {
-	T& buffer_;
+	T* buffer_;
 
 public:
-	PacketStream(T& buffer) : buffer_(buffer) {}
+	PacketStream(T* buffer) : buffer_(buffer) {}
 
 	inline PacketStream& operator <<(const Botan::SecureVector<Botan::byte>& data) {
 		for(auto b : data) {
@@ -26,15 +26,21 @@ public:
 	}
 
 	inline PacketStream& operator <<(const std::string& data) {
-		std::copy(data.begin(), data.end(), std::back_inserter(buffer_));
+		std::copy(data.begin(), data.end(), std::back_inserter(*buffer_));
 		return *this;
 	}
 
 	template<typename U>
 	inline PacketStream& operator <<(const U& data) {
 		const char* copy = static_cast<const char*>(static_cast<const void*>(&data));
-		std::copy(copy, copy + sizeof(data), std::back_inserter(buffer_));
+		std::copy(copy, copy + sizeof(data), std::back_inserter(*buffer_));
 		return *this;
+	}
+
+	inline T* swap(T* buffer) {
+		T* old = buffer_;
+		buffer_ = buffer;
+		return old;
 	}
 };
 
