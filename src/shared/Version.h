0249dd200e869f7e8344ee3f621bcdbeb3e10006
Chaosvex <ch@osvex.com>
2015-05-21 20:32:47 +0100

    Add missing header guard

--- src/shared/Version.h
+++ src/shared/Version.h
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2014 Ember
+ * Copyright (c) 2014, 2015 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+#pragma once 
 
 #include <string>
 
@@ -13,4 +15,4 @@ namespace ember { namespace version {
 extern const std::string GIT_HASH;
 extern const std::string VERSION;
 
-}}
+}} // version, ember
