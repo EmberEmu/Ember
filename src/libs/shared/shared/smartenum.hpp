dd5f84460b718c67b22a69f99718a1f8a439be39
Chaosvex <ch@osvex.com>
2015-12-01 05:20:55 +0000

    Formatting fixes

--- src/libs/shared/shared/smartenum.hpp
+++ src/libs/shared/shared/smartenum.hpp
@@ -1,25 +1,25 @@
 /*
-	The MIT License (MIT)
+The MIT License (MIT)
 
-	Copyright (c) 2015 Tobias Widlund
+Copyright (c) 2015 Tobias Widlund
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 #pragma once
@@ -85,12 +85,12 @@ namespace smart_enum
         return result;
     };
     
-	template<typename SizeType>
+    template<typename SizeType>
     inline std::unordered_map<SizeType, std::string> makeEnumNameMap(std::string enumValuesString)
     {
         std::unordered_map<SizeType, std::string> nameMap;
     
-		SizeType currentEnumValue = 0;
+        SizeType currentEnumValue = 0;
         while(enumValuesString != "")
         {
             std::string currentEnumEntry = extractEntry(enumValuesString);
@@ -104,13 +104,13 @@ namespace smart_enum
             }
     
             currentEnumEntry = trimWhitespace(currentEnumEntry);
-			
+            
 
             nameMap[currentEnumValue] = currentEnumEntry;
     
             currentEnumValue++;
         }
-		
+        
         return nameMap;
     }
     
@@ -119,7 +119,7 @@ namespace smart_enum
     {
         std::vector<Type> enumList;
     
-		SizeType currentEnumValue = 0;
+        SizeType currentEnumValue = 0;
         while(enumValuesString != "")
         {
             std::string currentEnumEntry = extractEntry(enumValuesString);
