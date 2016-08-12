402ac709e15e6531bc676faa6818a6ec7f12f984
Chaosvex <ch@osvex.com>
2016-08-12 20:01:35 +0100

    Rewrite the packet printer function

--- src/libs/shared/shared/util/FormatPacket.h
+++ src/libs/shared/shared/util/FormatPacket.h
@@ -8,31 +8,46 @@
 
 #pragma once 
 
-#include <sstream>
 #include <iomanip>
-#include <string>
+#include <sstream>
 #include <cstddef>
 #include <cctype>
 
 namespace ember { namespace util {
 
-inline std::string format_packet(unsigned char* packet, std::size_t size) {
-	std::stringstream hex, ascii, formatted;
-	hex << std::hex << std::setfill('0');
+inline std::string format_packet(unsigned char* packet, std::size_t size, int columns = 16) {
+	std::size_t rows = ceil(size / static_cast<double>(columns));
+	std::stringstream buffer;
+	auto offset = 0;
 
-	for(std::size_t i = 1; i != size; ++i) {
-		hex << std::setw(2) << static_cast<unsigned>(packet[i]) << " ";
-		ascii << (std::isprint(packet[i])? packet[i] : static_cast<unsigned char>('.'));
+	for(std::size_t i = 0; i != rows; ++i) {
+		buffer << std::hex << std::setw(4) << std::setfill('0') << i * columns << "   ";
 
-		if(!(i % 15)) {
-			formatted << hex.str() << "     " << ascii.str() << "\n";
-			hex.str(""); hex.clear();
-			ascii.str(""); ascii.clear();
-			hex << std::hex << std::setfill('0');
+		for(std::size_t j = 0; j < columns; ++j) {
+			buffer << std::hex << std::setfill('0');
+
+			if(j + offset < size) {
+				buffer << std::setw(2) << static_cast<unsigned>(packet[j + offset]) << " ";
+			} else {
+				buffer << "   ";
+			}
 		}
+
+		buffer << "     ";
+
+		for(std::size_t j = 0; j < columns; ++j) {
+			if(j + offset < size) {
+				buffer << (std::isprint(packet[j + offset]) ? packet[j + offset] : static_cast<unsigned char>('.'));
+			} else {
+				buffer << " ";
+			}
+		}
+
+		offset += columns;
+		buffer << "\n";
 	}
 
-	return formatted.str();
+	return buffer.str();
 }
 
 }} // util, ember
