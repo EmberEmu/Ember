c75a6178344037b8516639b0f062018c6bcd3055
Chaosvex <ch@osvex.com>
2016-09-12 15:33:36 +0100

    Small fixes

--- src/gateway/RealmService.cpp
+++ src/gateway/RealmService.cpp
@@ -32,12 +32,12 @@ void RealmService::handle_message(const spark::Link& link, const em::MessageRoot
 }
 
 void RealmService::set_realm_online() {
-	realm_.flags &= ~Realm::Flag::OFFLINE;
+	realm_.flags &= ~Realm::Flags::OFFLINE;
 	broadcast_realm_status();
 }
 
 void RealmService::set_realm_offline() {
-	realm_.flags |= Realm::Flag::OFFLINE;
+	realm_.flags |= Realm::Flags::OFFLINE;
 	broadcast_realm_status();
 }
 
