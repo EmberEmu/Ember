3f23899ca5183af98ca8c226883df8e36e526253
Chaosvex <chaosvex@touringnet.co.uk>
2017-11-15 19:21:30 +0000

    Additional comments
    Commented to highlight the grouping of opcodes by the service that would have originally handled them (maybe).

--- src/login/grunt/Opcodes.h
+++ src/login/grunt/Opcodes.h
@@ -14,12 +14,17 @@
 namespace ember::grunt {
 
 smart_enum_class(Opcode, std::uint8_t,
+	// Auth server opcodes
 	CMD_AUTH_LOGON_CHALLENGE      = 0x00,
 	CMD_AUTH_LOGON_PROOF          = 0x01,
 	CMD_AUTH_RECONNECT_CHALLENGE  = 0x02,
 	CMD_AUTH_RECONNECT_PROOF      = 0x03,
 	CMD_SURVEY_RESULT             = 0x04,
+	
+	// Realm listing server opcodes
 	CMD_REALM_LIST                = 0x10,
+
+	// Patch server opcodes
 	CMD_XFER_INITIATE             = 0x30,
 	CMD_XFER_DATA                 = 0x31,
 	CMD_XFER_ACCEPT               = 0x32,
