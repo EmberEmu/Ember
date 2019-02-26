4dd4457e2751144fe6d35b127fb0d018d70d0a5d
Thorbjørn H.H. Sindrup <holsthein@live.dk>
2019-02-27 00:49:10 +0100

    Create FindZlib.cmake
    Don't use build in cmake Findzlib since it doesn't actually fail on zlib not found and then it hides the ZLIB_INCLUDE_DIR and ZLIB_LIBRARY so you have to do a hard reload of cmake to get it working. Adding a FindZlib and doing the version control here as well as make sure to display the entry in cmake gui

---
+++ cmake/FindZlib.cmake
@@ -0,0 +1,48 @@
+# Module for locating the zlib library
+# Based on Peter Kapec's FindPCRE module as well as Sergiu Dotenco's FindBotan module
+
+# Look for the header file.
+FIND_PATH (ZLIB_INCLUDE_DIR NAMES zlib.h)
+
+# Look for the library.
+FIND_LIBRARY (ZLIB_LIBRARY NAMES zlib)
+
+IF (ZLIB_INCLUDE_DIR)
+  SET (_ZLIB_VERSION_HEADER ${ZLIB_INCLUDE_DIR}/zlib.h)
+
+  IF (EXISTS ${_ZLIB_VERSION_HEADER})
+    FILE (STRINGS ${_ZLIB_VERSION_HEADER} _ZLIB_VERSION_TMP REGEX
+      "#define ZLIB_VER_(MAJOR|MINOR|REVISION|SUBREVISION)[ \t]+[0-9]+")
+
+    STRING (REGEX REPLACE
+      ".*#define ZLIB_VER_MAJOR[ \t]+([0-9]+).*" "\\1" ZLIB_VER_MAJOR
+      ${_ZLIB_VERSION_TMP})
+    STRING (REGEX REPLACE
+      ".*#define ZLIB_VER_MINOR[ \t]+([0-9]+).*" "\\1" ZLIB_VER_MINOR
+      ${_ZLIB_VERSION_TMP})
+    STRING (REGEX REPLACE
+      ".*#define ZLIB_VER_REVISION[ \t]+([0-9]+).*" "\\1" ZLIB_VER_REVISION
+      ${_ZLIB_VERSION_TMP})
+    STRING (REGEX REPLACE
+      ".*#define ZLIB_VER_SUBREVISION[ \t]+([0-9]+).*" "\\1" ZLIB_VER_SUBREVISION
+      ${_ZLIB_VERSION_TMP})
+
+    SET (ZLIB_VERSION_COUNT 4)
+    SET (ZLIB_VERSION
+      ${ZLIB_VER_MAJOR}.${ZLIB_VER_MINOR}.${ZLIB_VER_REVISION}.${ZLIB_VER_SUBREVISION})
+  ENDIF (EXISTS ${_ZLIB_VERSION_HEADER})
+ENDIF (ZLIB_INCLUDE_DIR)
+
+# Handle the QUIETLY and REQUIRED arguments and set ZLIB_FOUND to TRUE if all listed variables are TRUE.
+INCLUDE (FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (ZLIB REQUIRED_VARS ZLIB_LIBRARY
+  ZLIB_INCLUDE_DIR VERSION_VAR ZLIB_VERSION)
+
+# Copy the results to the output variables.
+IF (ZLIB_FOUND)
+	SET (ZLIB_LIBRARIES ${ZLIB_LIBRARY})
+	SET (ZLIB_INCLUDE_DIRS ${ZLIB_INCLUDE_DIR})
+ELSE (ZLIB_FOUND)
+	SET (ZLIB_LIBRARIES)
+	SET (ZLIB_INCLUDE_DIRS)
+ENDIF (ZLIB_FOUND)
