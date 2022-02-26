eb4883e60f4e9df1c722e0d83d8e39ed619e0705
Chaosvex <ch@osvex.com>
2022-02-26 11:31:27 +0000

    Switch realm list to use atomic shared_ptr

--- src/login/RealmList.cpp
+++ src/login/RealmList.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - 2019 Ember
+ * Copyright (c) 2015 - 2022 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,38 +11,43 @@
 
 namespace ember {
 
-RealmList::RealmList(std::vector<Realm> realms) : realms_(std::make_shared<RealmMap>()) {
+RealmList::RealmList(std::vector<Realm>&& realms) : realms_(std::make_shared<RealmMap>()) {
 	add_realm(std::move(realms));
 }
 
 void RealmList::add_realm(std::vector<Realm> realms) {
+	// ensure consistency if we add from multiple workers (not a thread safety issue)
 	std::lock_guard<std::mutex> guard(lock_);
 
-	auto copy = std::make_shared<RealmMap>(*realms_);
+	auto realm_map = realms_.load();
+	auto copy = std::make_shared<RealmMap>(*realm_map);
 	
-	for(auto& r : realms) {
-		copy->emplace(r.id, std::move(r));
+	for(auto& realm : realms) {
+		(*copy)[realm.id] = std::move(realm);
 	}
 
 	realms_ = copy;
 }
 
 void RealmList::add_realm(Realm realm) {
+	// ensure consistency if we add from multiple workers (not a thread safety issue)
 	std::lock_guard<std::mutex> guard(lock_);
 
-	auto copy = std::make_shared<RealmMap>(*realms_);
+	auto realm_map = realms_.load();
+	auto copy = std::make_shared<RealmMap>(*realm_map);
 	(*copy)[realm.id] = std::move(realm);
 
 	realms_ = copy;
 }
 
-Realm RealmList::get_realm(std::uint32_t id) const {
-	std::lock_guard<std::mutex> guard(lock_);
-	return realms_->at(id);
+Realm RealmList::get_realm(const std::uint32_t id) const {
+	auto realms = realms_.load();
+	return realms->at(id);
 }
 
 auto RealmList::realms() const -> std::shared_ptr<const RealmMap> {
-	return realms_;
+	auto realms = realms_.load();
+	return realms;
 }
 
 } // ember
--- src/login/RealmList.h
+++ src/login/RealmList.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - 2018 Ember
+ * Copyright (c) 2015 - 2022 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <shared/Realm.h>
+#include <atomic>
 #include <memory>
 #include <mutex>
 #include <unordered_map>
@@ -20,11 +21,11 @@ namespace ember {
 typedef std::unordered_map<std::uint32_t, Realm> RealmMap;
 
 class RealmList final {
-	std::shared_ptr<const RealmMap> realms_;
+	std::atomic<std::shared_ptr<const RealmMap>> realms_;
 	mutable std::mutex lock_;
 
 public:
-	explicit RealmList(std::vector<Realm> realms);
+	explicit RealmList(std::vector<Realm>&& realms);
 	RealmList() : realms_(std::make_shared<RealmMap>()){}
 	void add_realm(std::vector<Realm> realms);
 	void add_realm(Realm realm);
