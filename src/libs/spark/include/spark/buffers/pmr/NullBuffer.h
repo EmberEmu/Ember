4f508b3e018a9fa1c601f91db21d3822f21f5e2a
Chaosvex <ch@osvex.com>
2024-07-22 11:51:46 +0100

    Make NullBuffer compatible with BinaryStream (non-PMR)

--- src/libs/spark/include/spark/buffers/pmr/NullBuffer.h
+++ src/libs/spark/include/spark/buffers/pmr/NullBuffer.h
@@ -9,26 +9,33 @@
 #pragma once
 
 #include <spark/buffers/pmr/BufferWrite.h>
-#include <stdexcept>
+#include <spark/buffers/SharedDefs.h>
+#include <spark/Exception.h>
 #include <cstddef>
 
 namespace ember::spark::io::pmr {
 
 class NullBuffer final : public BufferWrite {
 public:
-	void write(const void* source, std::size_t length) override {};
-	void reserve(const std::size_t length) override {};
-	std::size_t size() const override{ return 0; };
+	using size_type       = std::size_t;
+	using value_type      = std::byte;
+	using contiguous      = is_contiguous;
+	using seeking         = unsupported;
+
+	template<typename T> void write(const T& elem) {}
+	void write(const void* source, size_type length) override {};
+	template<typename T> void read(T* elem) {}
+	void read(void* destination, size_type length) {};
+	template<typename T> void copy(T* elem) const {}
+	void copy(void* destination, size_type length) const {};
+	void reserve(const size_type length) override {};
+	size_type size() const override{ return 0; };
 	bool empty() const override { return true; };
 	bool can_write_seek() const override { return false; }
 
 	void write_seek(const BufferSeek direction, const std::size_t offset) override {
-		throw std::logic_error("Don't do this on a NullBuffer"); 
+		throw exception("Don't do this on a NullBuffer"); 
 	};
-
-	std::byte& operator[](const std::size_t index) override {
-		throw std::logic_error("Don't do this on a NullBuffer"); 
-	}
 };
 
 } // pmr, io, spark, ember
