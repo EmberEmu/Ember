2517143186475abe38eb815f7a91d36dece8038f
Chaosvex <ch@osvex.com>
2024-08-24 12:16:45 +0100

    Split NetworkSession

--- src/login/NetworkSession.h
+++ src/login/NetworkSession.h
@@ -49,112 +49,13 @@ private:
 	bool write_in_progress_;
 
 	SessionManager& sessions_;
-	const std::string remote_address_;
 	log::Logger* logger_;
 	bool stopped_;
 
-	void read() {
-		auto self(shared_from_this());
-		auto tail = inbound_buffer_.back();
-
-		// if the buffer chain has no more space left, allocate & attach new node
-		if(!tail || !tail->free()) {
-			tail = inbound_buffer_.allocate();
-			inbound_buffer_.push_back(tail);
-		}
-
-		set_timer();
-
-		socket_.async_receive(boost::asio::buffer(tail->write_data(), tail->free()), 
-			create_alloc_handler(allocator_,
-			[this, self](boost::system::error_code ec, std::size_t size) {
-				if(stopped_) {
-					return;
-				}
-
-				timer_.cancel();
-
-				if(!ec) {
-					inbound_buffer_.advance_write(size);
-
-					if(handle_packet(inbound_buffer_)) {
-						read();
-					} else {
-						close_session();
-					}
-				} else if(ec != boost::asio::error::operation_aborted) {
-					close_session();
-				}
-			}
-		));
-	}
-
-	void write(WriteCallback cb) {
-		auto self(shared_from_this());
-		set_timer();
-
-		const spark::io::BufferSequence sequence(*outbound_front_);
-
-		socket_.async_send(sequence, create_alloc_handler(allocator_,
-			[this, self, cb = std::move(cb)](boost::system::error_code ec, std::size_t size) mutable {
-			outbound_front_->skip(size);
-
-			if(!ec) {
-				if(!outbound_front_->empty()) {
-					write(std::move(cb)); // entire buffer wasn't sent, hit gather-write limits?
-				} else {
-					std::swap(outbound_front_, outbound_back_);
-
-					if(!outbound_front_->empty()) {
-						write(std::move(cb));
-					} else { // all done!
-						write_in_progress_ = false;
-					
-						if(cb) {
-							cb();
-						}
-					}
-				}
-			} else if(ec != boost::asio::error::operation_aborted) {
-				close_session();
-			}
-		}));
-	}
-
-	void set_timer() {
-		auto self(shared_from_this());
-
-		timer_.expires_from_now(SOCKET_ACTIVITY_TIMEOUT);
-		timer_.async_wait([this, self](const boost::system::error_code& ec) {
-			timeout(ec);
-		});
-	}
-
-	void timeout(const boost::system::error_code& ec) {
-		if(ec || stopped_) { // if ec is set, the timer was aborted (session close / refreshed)
-			return;
-		}
-
-		LOG_DEBUG_FILTER(logger_, LF_NETWORK)
-			<< "Idle timeout triggered on " << remote_address() << LOG_ASYNC;
-
-		close_session();
-	}
-
-	void stop() {
-		auto self(shared_from_this());
-
-		boost::asio::post(socket_.get_executor(), [this, self] {
-			LOG_DEBUG_FILTER(logger_, LF_NETWORK)
-				<< "Closing connection to " << remote_address() << LOG_ASYNC;
-
-			stopped_ = true;
-			boost::system::error_code ec; // we don't care about any errors
-			socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
-			socket_.close(ec);
-			timer_.cancel();
-		});
-	}
+	void read();
+	void write(WriteCallback cb);
+	void set_timer();
+	void timeout(const boost::system::error_code& ec);
 
 public:
 	NetworkSession(SessionManager& sessions, boost::asio::ip::tcp::socket socket,
@@ -168,7 +69,7 @@ public:
 	                 write_in_progress_(false),
 	                 logger_(logger), stopped_(false) { }
 
-	virtual void start() {
+	void start() {
 		read();
 	}
 
@@ -180,36 +81,8 @@ public:
 		return remote_ep_.address().to_string();
 	}
 
-	virtual void close_session() {
+	void close_session() {
 		sessions_.stop(shared_from_this());
-	}
-
-	template<typename BufferType>
-	void write_chain(std::unique_ptr<BufferType> chain, WriteCallback cb) {
-		auto self(shared_from_this());
-
-		if(!socket_.is_open()) {
-			return;
-		}
-
-		set_timer();
-
-		spark::io::BufferSequence sequence(*chain);
-
-		socket_.async_send(sequence, create_alloc_handler(allocator_,
-			[&, self, chain = std::move(chain), cb = std::move(cb)](boost::system::error_code ec,
-			                                                        std::size_t size) mutable {
-				chain->skip(size);
-
-				if(ec && ec != boost::asio::error::operation_aborted) {
-					close_session();
-				} else if(!ec && chain->size()) {
-					write_chain(std::move(chain), std::move(cb)); 
-				} else if(!ec && cb) {
-					cb();
-				}
-			}
-		));
 	}
 
 	template<typename StreamSerialisable>
@@ -234,10 +107,9 @@ public:
 		return socket_.get_executor();
 	}
 
+	void stop();
 	virtual bool handle_packet(spark::io::pmr::Buffer& buffer) = 0;
 	virtual ~NetworkSession() = default;
-
-	friend class SessionManager;
 };
 
 } // ember
