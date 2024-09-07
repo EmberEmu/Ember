e44ba028b0b1965b5a118e8569c60de33c146629
Chaosvex <ch@osvex.com>
2024-09-07 03:08:14 +0100

    Fix missing entry in state LUT Oof.

--- src/gateway/states/StateLUT.cpp
+++ src/gateway/states/StateLUT.cpp
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2016 - 2020 Ember
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+* Copyright (c) 2016 - 2024 Ember
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 #include "StateLUT.h"
 #include "Authentication.h"
@@ -17,17 +17,19 @@
 namespace ember { 
 
 const event_handler update_event[] = {
-    &authentication::handle_event,
-    &character_list::handle_event,
+	&authentication::handle_event,
+	&character_list::handle_event,
 	&world_enter::handle_event,
-    &world::handle_event,
-    &session_close::handle_event
+	nullptr, // world transfer, unhandled
+	&world::handle_event,
+	&session_close::handle_event
 };
 
 const packet_handler update_packet[] = {
 	&authentication::handle_packet,
 	&character_list::handle_packet,
 	&world_enter::handle_packet,
+	nullptr, // world transfer, unhandled
 	&world::handle_packet,
 	&session_close::handle_packet
 };
@@ -36,6 +38,7 @@ const state_func exit_states[] = {
 	&authentication::exit,
 	&character_list::exit,
 	&world_enter::exit,
+	nullptr, // world transfer, unhandled
 	&world::exit,
 	&session_close::exit
 };
@@ -44,6 +47,7 @@ const state_func enter_states[] = {
 	&authentication::enter,
 	&character_list::enter,
 	&world_enter::enter,
+	nullptr, // world transfer, unhandled
 	&world::enter,
 	&session_close::enter
 };
