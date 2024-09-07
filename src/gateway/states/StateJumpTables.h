2db28df4516eb73179e8470c39c8b8c58a8a5353
Chaosvex <ch@osvex.com>
2024-09-07 07:01:09 +0100

    Improve jump table validation Thanks to BD-Calvin for the suggestion

--- src/gateway/states/StateJumpTables.h
+++ src/gateway/states/StateJumpTables.h
@@ -9,46 +9,72 @@
 #pragma once
 
 #include "ClientStates.h"
+#include "Authentication.h"
+#include "CharacterList.h"
+#include "SessionClose.h"
+#include "WorldForwarder.h"
+#include "WorldEnter.h"
+#include "WorldTransfer.h"
 #include <protocol/Opcodes.h>
 #include <array>
-#include <utility>
+#include <algorithm>
+
+#define VALIDATE_JUMP_ENTRIES(x) \
+	static_assert(std::none_of(x.begin(), x.end(), [](auto p) \
+		{ return p == nullptr;}), "Missing jump table entry");
 
 namespace ember { 
 
 struct ClientContext;
 struct Event;
 
+using state_func = void(*)(ClientContext&);
+using event_handler = void(*)(ClientContext&, const Event*);
+using packet_handler = void(*)(ClientContext&, protocol::ClientOpcode);
+
 template<typename T>
 using JumpTable = std::array<T, STATES_NUM>;
 
-/*
- * Wrapper that ensures any resizing of the jump tables without also
- * having added the new state pointers will prevent compilation rather
- * than allowing the tables to contain junk entries
- * 
- * Compilers can see optimise this away, so it has no runtime penalty
- */
-template<typename T>
-struct no_default {
-	T t;
-
-	template<typename ...Args>
-	constexpr auto operator()(Args&& ...args) const {
-		t(std::forward<Args>(args)...);
-	}
-
-	constexpr no_default(T t) : t(t) {};
-	no_default() = delete;
+constexpr JumpTable<event_handler> update_event {
+	&authentication::handle_event,
+	&character_list::handle_event,
+	&world_enter::handle_event,
+	&world_transfer::handle_event,
+	&world::handle_event,
+	&session_close::handle_event
 };
 
+constexpr JumpTable<packet_handler> update_packet {
+	&authentication::handle_packet,
+	&character_list::handle_packet,
+	&world_enter::handle_packet,
+	&world_transfer::handle_packet,
+	&world::handle_packet,
+	&session_close::handle_packet
+};
 
-using state_func = no_default<void(*)(ClientContext&)>;
-using event_handler = no_default<void(*)(ClientContext&, const Event*)>;
-using packet_handler = no_default<void(*)(ClientContext&, protocol::ClientOpcode)>;
+constexpr JumpTable<state_func> exit_states {
+	&authentication::exit,
+	&character_list::exit,
+	&world_enter::exit,
+	&world_transfer::exit,
+	&world::exit,
+	&session_close::exit
+};
 
-extern const JumpTable<state_func> enter_states;
-extern const JumpTable<state_func> exit_states;
-extern const JumpTable<packet_handler> update_packet;
-extern const JumpTable<event_handler> update_event;
+constexpr JumpTable<state_func> enter_states {
+	&authentication::enter,
+	&character_list::enter,
+	&world_enter::enter,
+	&world_transfer::enter,
+	&world::enter,
+	&session_close::enter
+};
+
+// ensure jump tables cannot be resized without new entries being added
+VALIDATE_JUMP_ENTRIES(enter_states);
+VALIDATE_JUMP_ENTRIES(exit_states);
+VALIDATE_JUMP_ENTRIES(update_event);
+VALIDATE_JUMP_ENTRIES(update_packet);
 
 } // ember
