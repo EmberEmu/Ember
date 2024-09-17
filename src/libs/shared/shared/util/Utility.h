4e57495d1561e1108605a6924674acf9f370b3b7
Chaosvex <ch@osvex.com>
2024-09-17 10:31:19 +0100

    Comment page_lock

--- src/libs/shared/shared/util/Utility.h
+++ src/libs/shared/shared/util/Utility.h
@@ -14,17 +14,43 @@
 #include <string_view>
 #include <cstddef>
 
+/*
+ * A small collection of random, mostly platform-specific,
+ * utility functions
+ */
 namespace ember::util {
 
 std::size_t max_consecutive(std::string_view name, bool case_insensitive = false,
                             const std::locale& locale = std::locale());
+
 void set_window_title(cstring_view title);
 
+/*
+ * Attempts to determine the maximum number of sockets
+ * the process is allowed to open.
+ */
 int max_sockets();
+
+/*
+ * Produces a string describing the maximum number of
+ * sockets the process is allowed to open.
+ */
 std::string max_sockets_desc();
 
+/*
+ * Converts a signal number to a string
+ * e.g. 2 -> SIGINT
+ * Values are platform dependent
+ */
 std::string sig_str(int signal);
 
+/*
+ * Locks pages to physical memory, preventing swapping out
+ * to disk. Behaviour is platform dependent and error checking
+ * must be done based on the platform. Not unit tested because
+ * it behaves differently across multiple versions of Windows,
+ * requiring the minimum working set size to be increased.
+ */
 bool page_lock(void* address, std::size_t length);
 bool page_unlock(void* address, std::size_t length);
 
