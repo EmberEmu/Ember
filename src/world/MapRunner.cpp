78970ca36927279c957334f85e90f8ebb1daf76e
Chaosvex <ch@osvex.com>
2024-09-28 00:14:49 +0100

    Add logging & comments to update loop

--- src/world/MapRunner.cpp
+++ src/world/MapRunner.cpp
@@ -10,6 +10,7 @@
 #include <logger/Logger.h>
 #include <shared/util/Timing.h>
 #include <chrono>
+#include <source_location>
 #include <thread>
 
 using namespace std::chrono_literals;
@@ -25,10 +26,33 @@ void update(std::chrono::milliseconds delta) {
 	
 }
 
+/* 
+ * We can't always guarantee the update rate on a server and may
+ * wish to slow it down as required, so the main update loop uses
+ * a variable rather than fixed time step. If a subsystem wishes
+ * to use a fixed time step, it can still do so by running its own
+ * timing loop within the update.
+ * 
+ * A monotonic clock is being used as we don't want any changes in
+ * system time (e.g. DST) to impact the game logic.
+ */
 void run(log::Logger& log) {
 	LOG_TRACE(log) << log_func << LOG_ASYNC;
 
 	auto timer_guard = util::set_time_period(1ms);
+
+	/*
+	 * If we can't increase timer resolution, the worst that happens
+	 * is that timers/sleep may overshoot and cause a drop in update
+	 * rate, but it shouldn't affect the game logic since it'll be
+	 * accounted for in the delta.
+	 */
+	if(!timer_guard.success()) {
+		const auto src = std::source_location::current();
+
+		LOG_ERROR_ASYNC(log, "{}:{} - failed to set time period", 
+						src.file_name(), src.line());
+	}
 
 	volatile bool stop = false; // temporary, prevent optimisation
 	auto previous = std::chrono::steady_clock::now() - UPDATE_DELTA;
@@ -43,6 +67,14 @@ void run(log::Logger& log) {
 		const auto end = std::chrono::steady_clock::now();
 		const auto update_time = end - begin;
 
+		/*
+		 * If we've finished updating but still have time to spare, we'll
+		 * sleep to use the rest of it up. We should have decent enough
+		 * timer resolution for this to be accurate enough and if not,
+		 * too bad, because platform-specific APIs run at the same resolution
+		 * 
+		 * Chrome, Firefox and so on pretty much do things the same way.
+		 */
 		if(update_time < UPDATE_DELTA) {
 			std::this_thread::sleep_for(UPDATE_DELTA - update_time);
 		}
