c1446005c7bef587f61e5c2d2e894479bb4af776
Chaosvex <ch@osvex.com>
2025-02-02 16:41:36 +0000

    Allowing setting console colour through ctor

--- src/libs/shared/shared/utility/ConsoleColour.h
+++ src/libs/shared/shared/utility/ConsoleColour.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - 2024 Ember
+ * Copyright (c) 2015 - 2025 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -24,6 +24,10 @@ class ConsoleColour final {
 	Colour original_;
 
 public:
+	ConsoleColour(Colour colour) : original_(save_output_colour()) {
+		set_output_colour(colour);
+	}
+
 	ConsoleColour() : original_(save_output_colour()) {}
 
 	~ConsoleColour() {
