bf30ec0706734d557a11bb3fcc94a6120e567fc0
Chaosvex <ch@osvex.com>
2025-03-01 01:15:19 +0000

    Pass buffer to const iterator by ref

--- src/libs/spark/include/spark/buffers/BufferSequence.h
+++ src/libs/spark/include/spark/buffers/BufferSequence.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 - 2024 Ember
+ * Copyright (c) 2016 - 2025 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -16,64 +16,65 @@ namespace ember::spark::io {
 
 template<typename BufferType>
 class BufferSequence {
-	const BufferType* buffer_;
+	const BufferType& buffer_;
 
 public:
-	BufferSequence(const BufferType& buffer) : buffer_(&buffer) { }
+	BufferSequence(const BufferType& buffer) : buffer_(buffer) { }
 
-class const_iterator {
-	using Node = typename BufferType::node_type;
+	class const_iterator {
+		using Node = typename BufferType::node_type;
 
-public:
-	const_iterator(const BufferType* buffer, const Node* curr_node)
-		: buffer_(buffer), curr_node_(curr_node) {}
+	public:
+		const_iterator(const BufferType& buffer, const Node* curr_node)
+			: buffer_(buffer),
+			  curr_node_(curr_node) {}
 
-	const_iterator& operator++() {
-		curr_node_ = curr_node_->next;
-		return *this;
-	}
+		const_iterator& operator++() {
+			curr_node_ = curr_node_->next;
+			return *this;
+		}
 
-	const_iterator operator++(int) {
-		const_iterator current(*this);
-		curr_node_ = curr_node_->next;
-		return current;
-	}
+		const_iterator operator++(int) {
+			const_iterator current(*this);
+			curr_node_ = curr_node_->next;
+			return current;
+		}
 
-	boost::asio::const_buffer operator*() const {
-		const auto buffer = buffer_->buffer_from_node(curr_node_);
-		return boost::asio::const_buffer(buffer->read_data(), buffer->size());
-	}
+		boost::asio::const_buffer operator*() const {
+			const auto buffer = buffer_.buffer_from_node(curr_node_);
+			return boost::asio::const_buffer(buffer->read_data(), buffer->size());
+		}
 
-	bool operator==(const const_iterator& rhs) const {
-		return curr_node_ == rhs.curr_node_;
-	}
+		bool operator==(const const_iterator& rhs) const {
+			return curr_node_ == rhs.curr_node_;
+		}
 
-	bool operator!=(const const_iterator& rhs) const {
-		return curr_node_ != rhs.curr_node_;
-	}
+		bool operator!=(const const_iterator& rhs) const {
+			return curr_node_ != rhs.curr_node_;
+		}
 
-	const_iterator& operator=(const_iterator&) = delete;
+		const_iterator& operator=(const_iterator&) = delete;
 
-#ifdef BUFFER_DEBUG
-	std::pair<const char*, std::size_t> get_buffer() {
-		auto buffer = buffer_->buffer_from_node(curr_node_);
-		return std::make_pair<char*, std::size_t>(
-			const_cast<char*>(reinterpret_cast<const char*>(buffer->read_data())), buffer->size()
-		);
-	}
-#endif
+	#ifdef BUFFER_DEBUG
+		std::pair<const char*, std::size_t> get_buffer() {
+			auto buffer = buffer_.buffer_from_node(curr_node_);
+			return std::make_pair<char*, std::size_t>(
+				const_cast<char*>(reinterpret_cast<const char*>(buffer->read_data())), buffer->size()
+			);
+		}
+	#endif
 
-private:
-	const BufferType* buffer_;
-	const Node* curr_node_;
-};
+	private:
+		const BufferType& buffer_;
+		const Node* curr_node_;
+	};
 
 const_iterator begin() const {
-	return const_iterator(buffer_, buffer_->root_.next);
+	return const_iterator(buffer_, buffer_.root_.next);
 }
 
 const_iterator end() const {
-	return const_iterator(buffer_, &buffer_->root_);
+	return const_iterator(buffer_, &buffer_.root_);
 }
 
 friend class const_iterator;
