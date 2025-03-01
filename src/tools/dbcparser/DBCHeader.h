34ed3e489cad141de7b77b8d079c4cdca0233aad
Chaosvex <ch@osvex.com>
2025-03-01 01:07:19 +0000

    Replace define with symbol

--- src/tools/dbcparser/DBCHeader.h
+++ src/tools/dbcparser/DBCHeader.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 - 2024 Ember
+ * Copyright (c) 2019 - 2025 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -17,7 +17,7 @@
 #include <cstddef>
 
 constexpr std::uint32_t DBC_MAGIC = ember::util::make_mcc("WDBC");
-#define DBC_HEADER_SIZE 20
+constexpr auto DBC_HEADER_SIZE = 20u;
 
 namespace be = boost::endian;
 
