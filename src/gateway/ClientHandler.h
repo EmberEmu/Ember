98dc088d0161955863331bfb6515c804f9c13878
Chaosvex <ch@osvex.com>
2025-03-04 21:37:58 +0000

    Remove unused concepts header

--- src/gateway/ClientHandler.h
+++ src/gateway/ClientHandler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 - 2024 Ember
+ * Copyright (c) 2016 - 2025 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -18,7 +18,6 @@
 #include <shared/ClientRef.h>
 #include <boost/asio/steady_timer.hpp>
 #include <boost/uuid/uuid.hpp>
-#include <concepts>
 #include <chrono>
 #include <memory>
 #include <optional>
