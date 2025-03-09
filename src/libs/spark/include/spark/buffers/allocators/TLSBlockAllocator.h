4572bef56e8493a139d5e08f6f8e73445461558e
Chaosvex <ch@osvex.com>
2025-03-09 21:18:09 +0000

    Enable compiler to remove thread ID field from metadata when it's not required

--- src/libs/spark/include/spark/buffers/allocators/TLSBlockAllocator.h
+++ src/libs/spark/include/spark/buffers/allocators/TLSBlockAllocator.h
@@ -14,6 +14,7 @@
 #include <memory>
 #include <new>
 #include <thread>
+#include <type_traits>
 #include <utility>
 #include <cassert>
 #include <cstddef>
@@ -29,10 +30,6 @@ enum class PagePolicy {
 	no_lock, lock
 };
 
-enum class ThreadPolicy {
-	none, same_thread
-};
-
 namespace {
 
 struct FreeBlock {
@@ -44,6 +41,10 @@ concept gt_zero = size > 0;
 
 template<typename _ty>
 concept gte_freeblock = sizeof(_ty) >= sizeof(FreeBlock);
+
+struct thread_tag{};
+struct enforce_same : thread_tag{};
+struct no_thread_policy: thread_tag{};
 
 /*
  * Basic fixed-size block allocator that preallocates a slab of memory
@@ -64,15 +65,21 @@ concept gte_freeblock = sizeof(_ty) >= sizeof(FreeBlock);
  * is deallocated from a different thread. Used by the TLS allocator, since
  * implementing the functionality there is messier (and slower).
  */
-template<typename _ty, std::size_t _elements, PagePolicy _policy = PagePolicy::no_lock,
-	ThreadPolicy _thread_policy = ThreadPolicy::none>
+template<typename _ty, std::size_t _elements,
+	PagePolicy _policy = PagePolicy::no_lock,
+	std::derived_from<thread_tag> ThreadPolicy = no_thread_policy
+>
 requires gt_zero<_elements> && gte_freeblock<_ty>
 struct Allocator {
+	using tid_type = std::conditional<
+		std::is_same_v<ThreadPolicy, enforce_same>, std::thread::id, std::monostate
+	>::type;
+
 	struct Block {
 		_ty obj;
 
-		struct Metadata {
-			std::thread::id thread_id;
+		struct {
+			[[no_unique_address]] tid_type thread_id;
 			bool using_new;
 		} meta;
 	};
@@ -81,7 +88,7 @@ struct Allocator {
 
 	std::array<char, block_size * _elements> storage_;
 	FreeBlock* head_ = nullptr;
-	std::thread::id thread_id_;
+	[[no_unique_address]] tid_type thread_id_;
 
 #ifdef _DEBUG_TLS_BLOCK_ALLOCATOR
 	std::size_t storage_active_count = 0;
@@ -90,8 +97,11 @@ struct Allocator {
 	std::size_t total_deallocs = 0;
 #endif
 
-	Allocator()	: thread_id_(std::this_thread::get_id())
-	{
+	Allocator()	{
+		if constexpr(std::is_same_v<ThreadPolicy, enforce_same>) {
+			thread_id_ = std::this_thread::get_id();
+		}
+
 		if constexpr(_policy == PagePolicy::lock) {
 			util::page_lock(storage_.data(), storage_.size());
 		}
@@ -148,7 +158,7 @@ struct Allocator {
 			block->meta.using_new = true;
 		}
 
-		if constexpr(_thread_policy == ThreadPolicy::same_thread) {
+		if constexpr(std::is_same_v<ThreadPolicy, enforce_same>) {
 			block->meta.thread_id = thread_id_;
 		}
 
@@ -159,7 +169,7 @@ struct Allocator {
 		assert(t);
 		auto block = std::start_lifetime_as<Block>(t);
 
-		if constexpr(_thread_policy == ThreadPolicy::same_thread) {
+		if constexpr(std::is_same_v<ThreadPolicy, enforce_same>) {
 			assert(block->meta.thread_id == thread_id_
 				&& "thread policy error or clobbered block");
 		}
@@ -198,7 +208,7 @@ struct Allocator {
 
 template<typename _ty, std::size_t _elements, PagePolicy policy = PagePolicy::lock>
 class TLSBlockAllocator final {
-	using AllocatorType = Allocator<_ty, _elements, policy, ThreadPolicy::same_thread>;
+	using AllocatorType = Allocator<_ty, _elements, policy, enforce_same>;
 
 	static inline thread_local std::unique_ptr<AllocatorType> allocator_;
 
