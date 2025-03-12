b4c380d6a895dfafdecadf6ec3e4293f545875cd
Chaosvex <ch@osvex.com>
2025-03-12 07:38:00 +0000

    Allow logging macros to be used within unbraced control blocks Each macro only contains a single statement within the condition, so no need for do while(0) tricks

--- src/libs/logger/include/logger/HelperMacros.h
+++ src/libs/logger/include/logger/HelperMacros.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2015 - 2024 Ember
+* Copyright (c) 2015 - 2025 Ember
 *
 * This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -15,119 +15,119 @@ inline auto& log_deref(auto* x) { return *x; }
 
 #if !NO_LOGGING && !NO_TRACE_LOGGING
 #define LOG_TRACE(logger) \
-	if(logger->severity() <= ember::log::Severity::TRACE) { \
+	if(logger->severity() <= ember::log::Severity::TRACE) \
 		log_deref(logger) << ember::log::Severity::TRACE
 #else
 #define LOG_TRACE(logger) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_DEBUG_LOGGING
 #define LOG_DEBUG(logger) \
-	if(logger->severity() <= ember::log::Severity::DEBUG) { \
+	if(logger->severity() <= ember::log::Severity::DEBUG) \
 		log_deref(logger) << ember::log::Severity::DEBUG
 #else
 #define LOG_DEBUG(logger) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_INFO_LOGGING
 #define LOG_INFO(logger) \
-	if(logger->severity() <= ember::log::Severity::INFO) { \
+	if(logger->severity() <= ember::log::Severity::INFO) \
 		log_deref(logger) << ember::log::Severity::INFO
 #else
 #define LOG_INFO(logger) \
-	if(false) {	\
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_WARN_LOGGING
 #define LOG_WARN(logger) \
-if(logger->severity() <= ember::log::Severity::WARN) { \
+if(logger->severity() <= ember::log::Severity::WARN) \
 		log_deref(logger) << ember::log::Severity::WARN
 #else
 #define LOG_WARN(logger) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_ERROR_LOGGING
 #define LOG_ERROR(logger) \
-	if(logger->severity() <= ember::log::Severity::ERROR_) { \
+	if(logger->severity() <= ember::log::Severity::ERROR_) \
 		log_deref(logger) << ember::log::Severity::ERROR_
 #else
 #define LOG_ERROR(logger) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 
 #if !NO_LOGGING && !NO_FATAL_LOGGING
 #define LOG_FATAL(logger) \
-	if(logger->severity() <= ember::log::Severity::FATAL) { \
+	if(logger->severity() <= ember::log::Severity::FATAL) \
 		log_deref(logger) << ember::log::Severity::FATAL
 #else
 #define LOG_FATAL(logger) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_TRACE_LOGGING
 #define LOG_TRACE_FILTER(logger, type) \
-	if(logger->severity() <= ember::log::Severity::TRACE && !(logger->filter() & type)) { \
+	if(logger->severity() <= ember::log::Severity::TRACE && !(logger->filter() & type)) \
 		log_deref(logger) << ember::log::Severity::TRACE << ember::log::Filter(type)
 #else
 #define LOG_TRACE_FILTER(logger, type) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_DEBUG_LOGGING
 #define LOG_DEBUG_FILTER(logger, type) \
-	if(logger->severity() <= ember::log::Severity::DEBUG && !(logger->filter() & type)) { \
+	if(logger->severity() <= ember::log::Severity::DEBUG && !(logger->filter() & type)) \
 		log_deref(logger) << ember::log::Severity::DEBUG << ember::log::Filter(type)
 #else
 #define LOG_DEBUG_FILTER(logger, type) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_INFO_LOGGING
 #define LOG_INFO_FILTER(logger, type) \
-	if(logger->severity() <= ember::log::Severity::INFO && !(logger->filter() & type)) { \
+	if(logger->severity() <= ember::log::Severity::INFO && !(logger->filter() & type)) \
 		log_deref(logger) << ember::log::Severity::INFO << ember::log::Filter(type)
 #else
 #define LOG_INFO_FILTER(logger, type) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_WARN_LOGGING
 #define LOG_WARN_FILTER(logger, type) \
-	if(logger->severity() <= ember::log::Severity::WARN && !(logger->filter() & type)) { \
+	if(logger->severity() <= ember::log::Severity::WARN && !(logger->filter() & type)) \
 		log_deref(logger) << ember::log::Severity::WARN << ember::log::Filter(type)
 #else
 #define LOG_WARN_FILTER(logger, type) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 #if !NO_LOGGING && !NO_ERROR_LOGGING
 #define LOG_ERROR_FILTER(logger, type) \
-	if(logger->severity() <= ember::log::Severity::ERROR_ && !(logger->filter() & type)) { \
+	if(logger->severity() <= ember::log::Severity::ERROR_ && !(logger->filter() & type)) \
 		log_deref(logger) << ember::log::Severity::ERROR_ << ember::log::Filter(type)
 #else
 #define LOG_ERROR_FILTER(logger, type) \
-	if(false) { \
+	if(false) \
 		log_deref(logger)
 #endif
 
 
 #if !NO_LOGGING && !NO_FATAL_LOGGING
 #define LOG_FATAL_FILTER(logger, type) \
-	if(logger->severity() <= ember::log::Severity::FATAL && !(logger->filter() & type)) { \
+	if(logger->severity() <= ember::log::Severity::FATAL && !(logger->filter() & type)) \
 		log_deref(logger) << ember::log::Severity::FATAL << ember::log::Filter(type)
 #else
 #define LOG_FATAL_FILTER(logger, type) \
@@ -171,8 +171,8 @@ if(logger->severity() <= ember::log::Severity::WARN) { \
 #define LOG_FATAL_FILTER_GLOB(filter) \
 	LOG_FATAL_FILTER(ember::log::global_logger(), filter)
 
-#define LOG_ASYNC ember::log::flush; }
-#define LOG_SYNC  ember::log::flush_sync; }
+#define LOG_ASYNC ember::log::flush
+#define LOG_SYNC  ember::log::flush_sync
 
 #if !NO_LOGGING && !NO_TRACE_LOGGING
 #define LOG_TRACE_ASYNC(logger, fmt_str, ...) \
