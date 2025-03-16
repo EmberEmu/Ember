352e25b9d6cbafcb0bef6d3bced233b573ff262c
Chaosvex <ch@osvex.com>
2025-03-16 13:28:19 +0000

    Add support for ARM yield() intrinsic in spinlock

--- src/libs/shared/shared/threading/Spinlock.h
+++ src/libs/shared/shared/threading/Spinlock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 - 2024 Ember
+ * Copyright (c) 2014 - 2025 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,11 +8,17 @@
 
 #pragma once
 
+#if defined(__x86_64__) || defined(_M_X64) || defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+#define ember_x86_or_x64
+#elif defined(__arm__) || defined(_M_ARM) || defined(_M_ARM64) || defined(_M_ARM64EC) || defined(__aarch64__) || defined(_M_ARM64)
+#define ember_arm
+#endif
+
 #include <atomic>
 #include <thread>
 #ifdef _MSC_VER
 #include <intrin.h>
-#else
+#elif defined (ember_x86_or_x64)
 #include <x86intrin.h>
 #endif
 
@@ -27,15 +33,12 @@ public:
 	Spinlock() : state(State::UNLOCKED) {}
 
 	inline bool acquire() {
+		// relaxed load before attempting the exchange helps is friendlier to shared cache
 		if(state.load(std::memory_order_relaxed) == State::LOCKED) {
 			return false;
 		}
 
-		if(state.exchange(State::LOCKED, std::memory_order_acquire) == State::UNLOCKED) {
-			return true;
-		} else {
-			return false;
-		}
+		return state.exchange(State::LOCKED, std::memory_order_acquire) == State::UNLOCKED;
 	}
 
 	void lock() {
@@ -44,7 +47,11 @@ public:
 				spins = 0;
 				std::this_thread::yield();
 			} else {
+#ifdef ember_x86_or_x64
 				_mm_pause();
+#elif defined(ember_arm)
+				__yield();
+#endif
 			}
 		}
 	}
