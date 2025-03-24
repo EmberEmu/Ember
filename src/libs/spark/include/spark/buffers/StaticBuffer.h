041ddbdc308edb88f72de1d2bc4011aceeb27280
Chaosvex <ch@osvex.com>
2025-03-24 08:27:17 +0000

    Change type aliases to reference std::array

--- src/libs/spark/include/spark/buffers/StaticBuffer.h
+++ src/libs/spark/include/spark/buffers/StaticBuffer.h
@@ -29,8 +29,8 @@ class StaticBuffer final {
 	std::size_t write_ = 0;
 
 public:
-	using size_type       = std::size_t;
-	using offset_type     = std::size_t;
+	using size_type       = decltype(buffer_)::size_type;
+	using offset_type     = offset_type;
 	using value_type      = StorageType;
 	using contiguous      = is_contiguous;
 	using seeking         = supported;
