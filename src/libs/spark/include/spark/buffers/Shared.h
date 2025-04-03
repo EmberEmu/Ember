91d470269476bc870ddd59a7c55050389a9b5f0d
Chaosvex <ch@osvex.com>
2025-04-03 15:24:45 +0100

    Simplify region_overlap

--- src/libs/spark/include/spark/buffers/Shared.h
+++ src/libs/spark/include/spark/buffers/Shared.h
@@ -109,17 +109,13 @@ constexpr auto generate_filled(const std::uint8_t value) {
 }
 
 // Returns true if there's any overlap between source and destination ranges
+[[maybe_unused]]
 static inline bool region_overlap(const void* src, std::size_t src_len, const void* dst, std::size_t dst_len) {
 	const auto src_beg = std::bit_cast<std::uintptr_t>(src);
 	const auto src_end = src_beg + src_len;
 	const auto dst_beg = std::bit_cast<std::uintptr_t>(dst);
 	const auto dst_end = dst_beg + dst_len;
-
-	// cannot assume src is before dst or vice versa
-	return (src_beg >= dst_beg && src_beg < dst_end)
-		|| (src_end > dst_beg && src_end <= dst_end)
-		|| (dst_beg >= src_beg && dst_beg < src_end)
-		|| (dst_end > src_beg && dst_end <= src_end);
+	return src_beg < dst_end && dst_beg < src_end;
 }
 
 } // detail
