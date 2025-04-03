26aabcec46170c619f79647632d6da6a79773b1e
Chaosvex <ch@osvex.com>
2025-04-03 14:51:56 +0100

    Mark fill() as constexpr

--- src/libs/spark/include/spark/buffers/BinaryStream.h
+++ src/libs/spark/include/spark/buffers/BinaryStream.h
@@ -225,7 +225,7 @@ public:
 	}
 
 	template<size_type size>
-	void fill(const std::uint8_t value) requires writeable<buf_type> {
+	constexpr void fill(const std::uint8_t value) requires writeable<buf_type> {
 		const auto filled = generate_filled<size>(value);
 		buffer_.write(filled.data(), filled.size());
 		total_write_ += size;
