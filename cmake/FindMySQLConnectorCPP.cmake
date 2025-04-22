984a737aca37d4f26483bc21403235486d20496f
Thorbjørn H.H. Sindrup <holsthein@live.dk>
2025-04-22 16:50:22 +0200

    Update FindMySQLConnectorCPP.cmake (#87)
    Accommodate vcpkg folder structure and library naming scheme when searching for mysql-connector-c++

--- cmake/FindMySQLConnectorCPP.cmake
+++ cmake/FindMySQLConnectorCPP.cmake
@@ -3,29 +3,46 @@
 
 include(FindPackageHandleStandardArgs)
 
+set(MYSQLCCPP_ROOT_HINTS
+    include/mysql_connection.h            # Docker
+    include/jdbc/mysql_connection.h)      # vcpkg
+set(MYSQLCCPP_INCLUDE_PATH
+    include                               # Docker
+    include/jdbc)                         # vcpkg
+
+set(MYSQLCCPP_LIBRARY_NAMES
+    mysqlcppconn                          # Docker
+    libmysqlcppconn-static.a)             # vcpkg
 set(_MYSQLCCPP_POSSIBLE_LIB_SUFFIXES lib)
 
+set(MYSQLCCPP_LIBRARYD_NAMES
+    mysqlcppconnd                         # Docker
+    libmysqlcppconn-static.a)             # vcpkg
+set(_MYSQLCCPP_POSSIBLE_LIBD_SUFFIXES
+    debug/lib                             # vcpkg
+    lib)                                  # Docker
+
 find_path(MYSQLCCPP_ROOT_DIR
-          NAMES include/mysql_connection.h
+          NAMES ${MYSQLCCPP_ROOT_HINTS}
           PATHS ENV MYSQLCCPPROOT
           DOC "MySQL Connector/C++ root directory")
 
 find_path(MYSQLCCPP_INCLUDE_DIR
           NAMES mysql_connection.h
           HINTS ${MYSQLCCPP_ROOT_DIR}
-          PATH_SUFFIXES include
+          PATH_SUFFIXES ${MYSQLCCPP_INCLUDE_PATH}
           DOC "MySQL Connector/C++ include directory")
 
 find_library(MYSQLCCPP_LIBRARY_RELEASE
-             NAMES mysqlcppconn
+             NAMES ${MYSQLCCPP_LIBRARY_NAMES}
              HINTS ${MYSQLCCPP_ROOT_DIR}
              PATH_SUFFIXES ${_MYSQLCCPP_POSSIBLE_LIB_SUFFIXES}
              DOC "MySQL Connector/C++ release library")
 
 find_library(MYSQLCCPP_LIBRARY_DEBUG
-             NAMES mysqlcppconnd
+             NAMES ${MYSQLCCPP_LIBRARYD_NAMES}
              HINTS ${MYSQLCCPP_ROOT_DIR}
-             PATH_SUFFIXES ${_MYSQLCCPP_POSSIBLE_LIB_SUFFIXES}
+             PATH_SUFFIXES ${_MYSQLCCPP_POSSIBLE_LIBD_SUFFIXES}
              DOC "MySQL Connector/C++ debug library")
 
 if(MYSQLCCPP_LIBRARY_DEBUG AND MYSQLCCPP_LIBRARY_RELEASE)
