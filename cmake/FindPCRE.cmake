30ee0fdff25183e75d4db8aafd86749b122c583f
Thorbjørn H.H. Sindrup <holsthein@live.dk>
2025-05-10 05:51:03 +0200

    Rewrite FindPCRE.cmake (#94)
    Complete rewrite of FindPCRE.cmake module from scratch;
    
    - Properly allocate both release and debug libraries
    - Automatically prefer static libraries if found
    - Properly check the set required version and use find_package_handle_standard_Args to throw an error if the version isn't matching or higher than requested
    - Report which library (or several) are in use and if they're static or shared

--- cmake/FindPCRE.cmake
+++ cmake/FindPCRE.cmake
@@ -1,38 +1,108 @@
-# Copyright (C) 2007-2009 LuaDist.
-# Created by Peter Kapec <kapecp@gmail.com>
-# Redistribution and use of this file is allowed according to the terms of the MIT license.
-# For details see the COPYRIGHT file distributed with LuaDist.
-#	Note:
-#		Searching headers and libraries is very simple and is NOT as powerful as scripts
-#		distributed with CMake, because LuaDist defines directories to search for.
-#		Everyone is encouraged to contact the author with improvements. Maybe this file
-#		becomes part of CMake distribution sometimes.
-
-# - Find pcre
-# Find the native PCRE headers and libraries.
+# Copyright (c) 2025 Ember
 #
-# PCRE_INCLUDE_DIRS	- where to find pcre.h, etc.
-# PCRE_LIBRARIES	- List of libraries when using pcre.
-# PCRE_FOUND	- True if pcre found.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Look for the header file.
-FIND_PATH(PCRE_INCLUDE_DIR NAMES pcre.h)
+SET(PCRE_POSSIBLE_STATICLIB_NAMES
+  pcre.lib                                  #         Win
+  pcre.a                                    # Linux/macOS
+  libpcre.lib                               #         Win (alternate)
+  libpcre.a)                                # Linux/macOS (alternate)
+SET(PCRE_POSSIBLE_LIBRARY_NAMES
+  pcre.dll                                  #         Win
+  pcre.so                                   #       Linux
+  pcre.dylib                                #       macOS
+  libpcre.dll                               #         Win (alternate)
+  libpcre.so                                #       Linux (alternate)
+  libpcre.dylib)                            #       macOS (alternate)
+SET(PCRE_POSSIBLE_LIB_SUFFIX lib)
+SET(PCRE_POSSIBLE_LIBD_SUFFIX debug/lib)
 
-# Look for the library.
-FIND_LIBRARY(PCRE_LIBRARY NAMES pcre)
+FIND_PATH(PCRE_INCLUDE_DIR                  # Look for the header file.
+  NAMES pcre.h
+  PATH_SUFFIXES include
+  DOC "pcre include directory")
 
-# Handle the QUIETLY and REQUIRED arguments and set PCRE_FOUND to TRUE if all listed variables are TRUE.
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(PCRE DEFAULT_MSG PCRE_LIBRARY PCRE_INCLUDE_DIR)
+FIND_LIBRARY(PCRE_STATICLIB_RELEASE         # Look for the static release library.
+  NAMES ${PCRE_POSSIBLE_STATICLIB_NAMES}
+  PATH_SUFFIXES ${PCRE_POSSIBLE_LIB_SUFFIX}
+  DOC "pcre static release library")
 
-# Copy the results to the output variables.
-IF(PCRE_FOUND)
-	SET(PCRE_LIBRARIES ${PCRE_LIBRARY})
-	SET(PCRE_INCLUDE_DIRS ${PCRE_INCLUDE_DIR})
-ELSE(PCRE_FOUND)
-	SET(PCRE_LIBRARIES)
-	SET(PCRE_INCLUDE_DIRS)
-ENDIF(PCRE_FOUND)
+FIND_LIBRARY(PCRE_STATICLIB_DEBUG           # Look for the static debug library.
+  NAMES ${PCRE_POSSIBLE_STATICLIB_NAMES}
+  PATH_SUFFIXES ${PCRE_POSSIBLE_LIBD_SUFFIX}
+  DOC "pcre static debug library")
 
-MARK_AS_ADVANCED(PCRE_INCLUDE_DIRS PCRE_LIBRARIES)
+# Set the library with a preference for a full static set
+IF(PCRE_STATICLIB_RELEASE)                  # Found static release
+  IF(PCRE_STATICLIB_DEBUG)                  # Found static   debug
+    SET(PCRE_LIBRARY                        # Default to static libraries if available.
+      optimized ${PCRE_STATICLIB_RELEASE}
+      debug ${PCRE_STATICLIB_DEBUG}
+      CACHE INTERNAL "PCRE static release and debug libraries")
+  ELSE()
+    SET(PCRE_LIBRARY ${PCRE_STATICLIB_RELEASE} CACHE INTERNAL "PCRE static release library")
+  ENDIF()
+ELSE()                                      # Static libraries unavailable - fallback on shared
+  FIND_LIBRARY(PCRE_LIBRARY_RELEASE         # Look for the shared release library.
+    NAMES ${PCRE_POSSIBLE_LIBRARY_NAMES}
+    PATH_SUFFIXES ${PCRE_POSSIBLE_LIB_SUFFIX}
+    DOC "pcre release library")
+  
+  FIND_LIBRARY(PCRE_LIBRARY_DEBUG           # Look for the shared debug library.
+    NAMES ${PCRE_POSSIBLE_LIBRARY_NAMES}
+    PATH_SUFFIXES ${PCRE_POSSIBLE_LIBD_SUFFIX}
+    DOC "pcre debug library")
 
+  IF(PCRE_LIBRARY_RELEASE)                  # Found shared release
+    IF(PCRE_LIBRARY_DEBUG)                  # Found shared   debug
+      SET(PCRE_LIBRARY                      # Fallback to shared library if available.
+        optimized ${PCRE_LIBRARY_RELEASE}
+        debug ${PCRE_LIBRARY_DEBUG}
+        CACHE INTERNAL "PCRE shared release and debug libraries")
+    ELSE()
+      SET(PCRE_LIBRARY ${PCRE_LIBRARY_RELEASE} CACHE INTERNAL "PCRE shared release library")
+    ENDIF()
+  ELSE()                                    # No library available in any expected path.
+    MESSAGE(FATAL_ERROR "pcre.h found but the library is unavailable in any of the expected paths.")
+  ENDIF()
+ENDIF()
+
+# Extract the version from the header and compare it to the required version.
+IF(PCRE_INCLUDE_DIR)
+  SET(_PCRE_VERSION_HEADER "${PCRE_INCLUDE_DIR}/pcre.h")
+  IF(EXISTS "${_PCRE_VERSION_HEADER}")
+    FILE(STRINGS "${_PCRE_VERSION_HEADER}" _PCRE_VERSION_TMP REGEX "#define PCRE_(MAJOR|MINOR)[ \t]+[0-9]+")
+    STRING(REGEX REPLACE ".*#define PCRE_MAJOR[ \t]+([0-9]+).*" "\\1" _PCRE_FOUND_MAJOR "${_PCRE_VERSION_TMP}")
+    STRING(REGEX REPLACE ".*#define PCRE_MINOR[ \t]+([0-9]+).*" "\\1" _PCRE_FOUND_MINOR "${_PCRE_VERSION_TMP}")
+    SET(PCRE_FOUND_VERSION "${_PCRE_FOUND_MAJOR}.${_PCRE_FOUND_MINOR}")
+    IF(PCRE_FOUND_VERSION VERSION_LESS "${PCRE_VERSION}")
+      MESSAGE(FATAL_ERROR "Your version of PCRE is ${PCRE_FOUND_VERSION} - This project requires at least ${PCRE_VERSION}")
+    ELSE()
+      SET(PCRE_VERSION "${PCRE_FOUND_VERSION}")
+    ENDIF()
+  ELSE()
+    MESSAGE(FATAL_ERROR "Unknown pcre version or pcre.h unavailable in any of the expected paths.")
+  ENDIF()
+ENDIF()
+
+# Handle the QUIETLY and REQUIRED arguments and mark PCRE as found if all listed variables are TRUE.
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PCRE REQUIRED_VARS PCRE_INCLUDE_DIR PCRE_LIBRARY VERSION_VAR PCRE_VERSION)
+
+# Report which library (set) is being utilized
+IF(PCRE_STATICLIB_DEBUG)
+  MESSAGE(STATUS "Using PCRE static release and debug libraries.")
+ELSEIF(PCRE_STATICLIB_RELEASE)
+  MESSAGE(STATUS "Using PCRE static release library.")
+ELSEIF(PCRE_LIBRARY_DEBUG)
+  MESSAGE(STATUS "Using PCRE shared release and debug libraries.")
+ELSEIF(PCRE_LIBRARY_RELEASE)
+  MESSAGE(STATUS "Using PCRE shared release library.")
+ENDIF()
+
+# Hide internally used variables except for PCRE_LIBRARY and PCRE_INCLUDE_DIR
+MARK_AS_ADVANCED(PCRE_POSSIBLE_STATICLIB_NAMES PCRE_POSSIBLE_LIBRARY_NAMES PCRE_POSSIBLE_LIB_SUFFIX
+                 PCRE_POSSIBLE_LIBD_SUFFIX PCRE_STATICLIB_RELEASE PCRE_STATICLIB_DEBUG PCRE_LIBRARY_RELEASE 
+                 PCRE_LIBRARY_DEBUG _PCRE_VERSION_HEADER _PCRE_VERSION_TMP _PCRE_FOUND_MAJOR _PCRE_FOUND_MINOR 
+                 PCRE_FOUND_VERSION PCRE_VERSION)
