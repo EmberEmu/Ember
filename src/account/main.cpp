252e488081934ff67707f1a40cc07b01c1ff3d19
Chaosvex <ch@osvex.com>
2026-02-06 18:25:46 +0000

    Clear signals before stopping services
    Although there's probably a reason I did it the other way.

--- src/account/main.cpp
+++ src/account/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - 2025 Ember
+ * Copyright (c) 2015 - 2026 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -64,8 +64,8 @@ int run(const po::variables_map& args, log::Logger& logger) try {
 
 	signals.async_wait([&](auto error, auto signal) {
 		LOG_DEBUG_SYNC(logger, "Received signal {}({})", util::sig_str(signal), signal);
-		service.stop();
 		signals.clear();
+		service.stop();
 	});
 
 	std::jthread worker([&]() {
--- src/gateway/main.cpp
+++ src/gateway/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - 2025 Ember
+ * Copyright (c) 2015 - 2026 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -63,10 +63,10 @@ int run(const po::variables_map& args, log::Logger& logger) try {
 
 	gateway::Service service(logger);
 
-	signals.async_wait([&](auto error, auto signal) {
+	signals.async_wait([&](auto /*error*/, auto signal) {
 		LOG_DEBUG_SYNC(logger, "Received signal {}({})", util::sig_str(signal), signal);
-		service.stop();
 		signals.clear();
+		service.stop();
 	});
 
 	std::jthread worker([&]() {
