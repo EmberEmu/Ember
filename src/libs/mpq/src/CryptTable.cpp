b7891c1c08178fc87bffa79672978bf14ad7e549
Chaosvex <ch@osvex.com>
2026-02-06 16:23:37 +0000

    Split MPQ crypto table to help (non-LLVM) optimisers

---
+++ src/libs/mpq/src/CryptTable.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 Ember
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <mpq/CryptTable.h>
+
+namespace ember::mpq {
+
+const std::invoke_result<decltype(&crypt_table)>::type CRYPT_TABLE = crypt_table();
+
+} // mpq, ember
