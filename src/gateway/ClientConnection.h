e99e17f86a5604c116a4f170fbe7dc55607bbd04
Chaosvex <ch@osvex.com>
2026-02-28 21:58:24 +0000

    Small formatting update

--- src/gateway/ClientConnection.h
+++ src/gateway/ClientConnection.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 - 2025 Ember
+ * Copyright (c) 2016 - 2026 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -90,20 +90,20 @@ class ClientConnection final {
 
 public:
 	ClientConnection(SessionManager& sessions, tcp_socket socket, ClientRef uuid, log::Logger& logger)
-		: sessions_(sessions),
-		  socket_(std::move(socket)),
-		  remote_ep_(socket_.remote_endpoint()),
-		  stats_{},
-		  msg_size_{0},
-		  logger_(logger),
-		  read_state_(ReadState::header),
-		  stopped_(false),
-		  write_in_progress_(false),
-		  handler_(*this, uuid, socket_.get_executor(), logger),
-		  compression_level_(0),
-		  outbound_front_(&outbound_buffers_.front()),
-		  outbound_back_(&outbound_buffers_.back()),
-		  stopping_(false) { 
+		: sessions_(sessions)
+		, socket_(std::move(socket))
+		, remote_ep_(socket_.remote_endpoint())
+		, stats_{}
+		, msg_size_{0}
+		, logger_(logger)
+		, read_state_(ReadState::header)
+		, stopped_(false)
+		, write_in_progress_(false)
+		, handler_(*this, uuid, socket_.get_executor(), logger)
+		, compression_level_(0)
+		, outbound_front_(&outbound_buffers_.front())
+		, outbound_back_(&outbound_buffers_.back())
+		, stopping_(false) { 
 		start();
 	}
 
