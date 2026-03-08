ae67c08eb5e7dc6db2ec6de49c735cc4685e62e2
Chaosvex <901914+Chaosvex@users.noreply.github.com>
2026-03-08 21:18:21 +0000

    Specify stream endian mode for DNS parsing rather than using boost::endian (#188)
    May as well use this functionality since I went to the effort of adding it :)

---
+++ src/mdns/StreamType.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026 Ember
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <spark/buffers/BufferAdaptor.h>
+#include <spark/buffers/BinaryStream.h>
+#include <span>
+#include <vector>
+
+namespace ember::dns {
+
+using ConstBufferAdaptor = spark::io::BufferAdaptor<std::span<const std::uint8_t>>;
+
+using StreamReadBigEndian = spark::io::BinaryStream<
+	ConstBufferAdaptor ,
+	spark::io::allow_throw_t,
+	spark::io::endian::as_big_t
+>;
+
+using BufferAdaptor = spark::io::BufferAdaptor<std::vector<std::uint8_t>>;
+
+using StreamWriteBigEndian = spark::io::BinaryStream<
+	BufferAdaptor,
+	spark::io::allow_throw_t,
+	spark::io::endian::as_big_t
+>;
+
+};
