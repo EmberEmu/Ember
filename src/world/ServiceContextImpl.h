f11eea3c9819c3b1a9891ce60c9eadba8e02a363
Chaosvex <ch@osvex.com>
2026-03-11 00:23:03 +0000

    World launch refactor

---
+++ src/world/ServiceContextImpl.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2026 Ember
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <spark/Server.h>
+#include <dbcreader/Storage.h>
+#include <conpool/ConnectionPool.h>
+#include <conpool/drivers/AutoSelect.h>
+#include <shared/database/daos/CharacterDAO.h>
+#include <thread/ThreadPool.h>
+#include <spark/Server.h>
+#include <memory>
+
+namespace ember::world {
+
+struct ServiceContext::Impl {
+	std::unique_ptr<dbc::Storage> dbcs;
+	std::unique_ptr<thread::ThreadPool> thread_pool;
+	std::unique_ptr<connection_pool::Pool<drivers::DriverType>> conn_pool;
+	std::unique_ptr<dal::CharacterDAO> character_dao;
+	std::unique_ptr<spark::Server> spark;
+};
+
+} // character, ember
