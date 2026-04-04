5983ec4ca9b1ffd2443c704c6eb6a2de7de3c6f2
Chaosvex <ch@osvex.com>
2026-04-04 05:59:02 +0100

    Missing include - will get there eventually

--- src/mdns/DNSDefines.h
+++ src/mdns/DNSDefines.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 - 2024 Ember
+ * Copyright (c) 2021 - 2026 Ember
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -10,8 +10,10 @@
 
 #include <shared/smartenum.hpp>
 #include <array>
+#include <string>
 #include <string_view>
 #include <variant>
+#include <vector>
 #include <cstdint>
 
 namespace ember::dns {
