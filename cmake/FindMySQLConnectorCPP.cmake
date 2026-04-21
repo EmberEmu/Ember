784f639968091ae9b89e67f7f7078bbcf700eb40
Chaosvex <ch@osvex.com>
2026-04-21 12:58:53 +0100

    Allow FindMySQLConnectorCPP to work with vcpkg on Windows

--- cmake/FindMySQLConnectorCPP.cmake
+++ cmake/FindMySQLConnectorCPP.cmake
@@ -12,12 +12,14 @@ set(MYSQLCCPP_INCLUDE_PATH
 
 set(MYSQLCCPP_LIBRARY_NAMES
     mysqlcppconn                          # Docker
-    libmysqlcppconn-static.a)             # vcpkg
+    mysqlcppconn-static                   # vcpkg (Windows)
+    libmysqlcppconn-static.a)             # vcpkg (Linux)
 set(_MYSQLCCPP_POSSIBLE_LIB_SUFFIXES lib)
 
 set(MYSQLCCPP_LIBRARYD_NAMES
     mysqlcppconnd                         # Docker
-    libmysqlcppconn-static.a)             # vcpkg
+    mysqlcppconn-static                   # vcpkg (Windows)
+    libmysqlcppconn-static.a)             # vcpkg (Linux)
 set(_MYSQLCCPP_POSSIBLE_LIBD_SUFFIXES
     debug/lib                             # vcpkg
     lib)                                  # Docker
@@ -61,3 +63,12 @@ mark_as_advanced(MYSQLCCPP_ROOT_DIR MYSQLCCPP_INCLUDE_DIR MYSQLCCPP_LIBRARY
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(MySQLConnectorCPP REQUIRED_VARS MYSQLCCPP_ROOT_DIR
   MYSQLCCPP_INCLUDE_DIR MYSQLCCPP_LIBRARY)
+
+# When using vcpkg, pull in the connector-jdbc imported target so its
+# transitive deps (libmysql, openssl, zlib, lz4, zstd, protobuf, dnsapi)
+# get linked alongside mysqlcppconn-static.lib.
+find_package(unofficial-mysql-connector-cpp CONFIG QUIET)
+if(TARGET unofficial::mysql-connector-cpp::connector-jdbc)
+    list(APPEND MYSQLCCPP_LIBRARY unofficial::mysql-connector-cpp::connector-jdbc)
+    list(APPEND MYSQLCCPP_LIBRARIES unofficial::mysql-connector-cpp::connector-jdbc)
+endif()
