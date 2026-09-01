75bbec6e92c4c735601ced48bfda4391456f6515
Chaosvex <ch@osvex.com>
2026-09-01 17:54:02 +0100

    Replace missed static_vector instance

--- src/login/PINAuthenticator.h
+++ src/login/PINAuthenticator.h
@@ -9,7 +9,7 @@
 #pragma once
 
 #include <shared/utility/Clock.h>
-#include <boost/container/static_vector.hpp>
+#include <shared/utility/polyfill/inplace_vector>
 #include <array>
 #include <span>
 #include <string_view>
@@ -31,7 +31,7 @@ public:
 
 private:
 	std::array<std::uint8_t, GRID_SIZE> remapped_grid;
-	boost::container::static_vector<std::uint8_t, MAX_PIN_LENGTH> pin_bytes_;
+	std::inplace_vector<std::uint8_t, MAX_PIN_LENGTH> pin_bytes_;
 
 	void pin_to_ascii();
 	void remap_pin_grid(std::uint32_t seed);
