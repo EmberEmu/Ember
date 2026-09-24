0a9f77e52262bbd7f752a9ab3db0a0682c4f68c4
Chaosvex <ch@osvex.com>
2026-09-24 19:19:55 +0100

    Update total write for non-absolute stream write seeks

--- src/libs/spark/include/spark/buffers/BinaryStream.h
+++ src/libs/spark/include/spark/buffers/BinaryStream.h
@@ -803,6 +803,14 @@ public:
 			total_write_ = offset;
 		} else {
 			buffer_.write_seek(static_cast<BufferSeek>(direction), offset);
+			
+			if(direction == StreamSeek::sk_forward) {
+				assert(offset <= std::numeric_limits<size_type>::max() - total_write_);
+				total_write_ += offset;
+			} else {
+				assert(offset <= total_write_);
+				total_write_ -= offset;
+			}
 		}
 	}
 
